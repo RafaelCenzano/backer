cb8ff68e8370c81383971f2e2e855ed7fe990a92
Savagecoder77 <savagecoder77@gmail.com>
2020-03-25 17:20:35 -0700

    make a class object

--- exampleConfig.py
+++ exampleConfig.py
@@ -6,17 +6,19 @@ Config for backer
 create empty lists for things you don't need or want
 '''
 
-# Folder to store backups
-BACKUP_FOLDER = os.path.join('Library', 'Backup')
+class Config:
+    
+    # Folder to store backups
+    BACKUP_FOLDER = os.path.join('Library', 'Backup')
 
-# Folders to backup
-FOLDERS_TO_BACKUP = [os.path.join('Users', 'jimdoe', 'Documents'), os.path.join('Users', 'jimdoe', 'Downloads'), os.path.join('Users', 'jimdoe', 'Desktop')]
+    # Folders to backup
+    FOLDERS_TO_BACKUP = [os.path.join('Users', 'jimdoe', 'Documents'), os.path.join('Users', 'jimdoe', 'Downloads'), os.path.join('Users', 'jimdoe', 'Desktop')]
 
-# Files to backup
-FILES_TO_BACKUP = []
+    # Files to backup
+    FILES_TO_BACKUP = []
 
-# Folders to not zip
-FOLDER_TO_NOT_ZIP = [os.path.join('Users', 'jimdoe', 'Documents', 'thing')]
+    # Folders to not zip
+    FOLDER_TO_NOT_ZIP = [os.path.join('Users', 'jimdoe', 'Documents', 'thing')]
 
-# Files to not zip
-FILE_TO_NOT_ZIP = [os.path.join('Users', 'jimdoe', 'Documents', 'file.txt')]
+    # Files to not zip
+    FILE_TO_NOT_ZIP = [os.path.join('Users', 'jimdoe', 'Documents', 'file.txt')]
