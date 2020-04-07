08e7209dd35a1acb0bf030d283d81c6cf8e0bb61
Savagecoder77 <savagecoder77@gmail.com>
2020-04-07 11:27:50 -0700

    create loop and scheduler to run function at a set time everyday

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import main
 import config
+from time import sleep as delay
 
 '''
 Run file
@@ -11,4 +12,14 @@ configurations = config.Config()
 # Create the run object
 runObject = main.Core(configurations)
 
-runObject.zipArchive()
+# Main loop
+while True:
+
+    now = datetime.datetime.now()
+    target = datetime.datetime.combine(datetime.date.today(), configurations.TIME_TO_ZIP)
+    
+    if target < now:
+        target += datetime.timedelta(days=1)
+
+    delay((target - now).total_seconds())
+    runObject.zipArchive()
