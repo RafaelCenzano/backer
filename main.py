063abee50fe6514429843043d6b7a21fdea79490
Savagecoder77 <savagecoder77@gmail.com>
2020-04-23 12:20:03 -0700

    new code for zipping

--- main.py
+++ main.py
@@ -14,7 +14,33 @@ class Core:
 
     def zipArchive(self):
 
-        zipper = zipfile.ZipFile('item.zip', 'w', zipfile.ZIP_DEFLATED)
+        startZip = datetime.datetime.now()
+
+        folders = []
+
+        for pathItems in self.config.FOLDERS_TO_ZIP:
+            folders.append('/' + pathItems + '/')
+
+        todayFormatted = repr(startZip.month) + '-' + repr(startZip.day) + '-' + repr(startZip.year)
+
+        archiveName = os.path.join(self.config.BACKUP_FOLDER, 'day', self.config.BACKUP_NAME + '_' + todayFormatted + '.zip')
+
+        zipper = zipfile.ZipFile(archiveName, 'w', compression=zipfile.ZIP_DEFLATED)
+
+        for pathThing in folders:
+            for root, dirs, files in os.walk(pathThing):
+                for things in files:
+                    currentPath = os.path.join(root, things)
+                    if '/Users/rafael/Documents/3D printing/' not in currentPath:
+                        if os.path.islink(currentPath):
+                            print(f'skipping : {currentPath}')
+                        else:
+                            if currentPath != '/Users/rafael/Documents/Programming/notebooks/testing/item.zip':
+                                print(f'zipping : {currentPath}')
+                                zipper.write(currentPath)
 
         zipper.close()
+
+        endZip = datetime.datetime.now()
+        print(end - start)
     
